d6fb3a546263a4227170b78fb855698c17c1c427
Irina Saveleva <fomina_88@mail.ru>
2021-12-13 23:14:06 +0400

    create junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,41 +1,42 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        @Test
-        public void shouldCalculateHowMuchBuy() {
-            int actual = cashbackHackService.remain(100);
-            int expected = 900;
-            assertEquals(actual,expected);
-        }
-        @Test
-        public void shouldCalculateHowMuchBuy2() {
-            //В этом тесте ошибка
-            int actual = cashbackHackService.remain(1000);
-            int expected = 0;
-            assertEquals(actual,expected);
-        }
-        @Test
-        public void shouldCalculateHowMuchBuy3() {
-            int actual = cashbackHackService.remain(0);
-            int expected = 1000;
-            assertEquals(actual,expected);
-        }
-        @Test
-        public void shouldCalculateHowMuchBuy4() {
-            int actual = cashbackHackService.remain(1300);
-            int expected = 700;
-            assertEquals(actual,expected);
-        }
-        @Test
-        public void shouldCalculateHowMuchBuy5() {
-            int actual = cashbackHackService.remain(955);
-            int expected = 45;
-            assertEquals(actual,expected);
-        }
-
+    CashbackHackService cashbackHackService = new CashbackHackService();
+    @Test
+    public void shouldCalculateHowMuchBuy() {
+        int actual = cashbackHackService.remain(300);
+        int expected = 700;
+        assertEquals(expected,actual);
     }
+
+    @Test
+    public void shouldCalculateHowMuchBuy2() {
+        //В этом тесте ошибка
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldCalculateHowMuchBuy3() {
+        int actual = cashbackHackService.remain(0);
+        int expected = 1000;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldCalculateHowMuchBuy4() {
+        int actual = cashbackHackService.remain(1800);
+        int expected = 200;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldCalculateHowMuchBuy5() {
+        int actual = cashbackHackService.remain(744);
+        int expected = 256;
+        assertEquals(expected,actual);
+    }
+
+}
